4dd5c85ee8cc69966deedd275537a66031de47ef
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-02-10 09:19:15 +0000

    JS WIP explore

--- discohash.cpp
+++ discohash.cpp
@@ -1,7 +1,3 @@
-// also known as BEBB4185
-// Copyright 2020 Cris Stringfellow
-// Licensed under GPL-3.0
-// https://github.com/cris691/discohash
 #include <cstdio>
 #include <inttypes.h>
 #include "discohash.h"
@@ -23,9 +19,10 @@ const int STATE64 = STATE >> 3;
 const int STATEM = STATE-1;
 const int HSTATE64M = (STATE64 >> 1)-1;
 const int STATE64M = STATE64-1;
+const uint64_t P = 0xFFFFFFFFFFFFFFFF - 58;
+const uint64_t Q = 13166748625691186689U;
+
 uint8_t disco_buf[STATE] = {0};
-uint64_t P = 0xFFFFFFFFFFFFFFFF - 58;
-uint64_t Q = 13166748625691186689U;
 uint8_t *ds8 = (uint8_t *)disco_buf;
 uint32_t *ds32 = (uint32_t *)disco_buf;
 uint64_t *ds = (uint64_t *)disco_buf;
@@ -41,14 +38,6 @@ uint64_t *ds = (uint64_t *)disco_buf;
       return v; 
     }
 
-    FORCE_INLINE uint64_t rot32( uint64_t v, int n) 
-    {
-      n = n & 31U;
-      if (n)
-          v = (v >> n) | (v << (64-n));
-      return v; 
-    }
-
     FORCE_INLINE uint8_t rot8( uint8_t v, int n) 
     {
       n = n & 7U;
@@ -57,17 +46,7 @@ uint64_t *ds = (uint64_t *)disco_buf;
       return v; 
     }
 
-    FORCE_INLINE void mixA()
-    {
-      int i = ds32[0] & 1;
-      int j = ds32[3] & 3;
-
-      ds[0] = rot(ds[0], ds[i]);
-      ds[i] *= (P % (ds32[j] + 1) + 1);
-      ds[1] += ds32[j];
-    }
-
-    FORCE_INLINE void mix(const int A)
+    void mix(const int A)
     {
       const int B = A+1;
       ds[A] *= P;
@@ -86,7 +65,7 @@ uint64_t *ds = (uint64_t *)disco_buf;
   //---------
   // Hash round function 
 
-    FORCE_INLINE void round( const uint64_t * m64, const uint8_t * m8, int len )
+    void round( const uint64_t * m64, const uint8_t * m8, int len )
     {
       int index = 0;
       int sindex = 0;
@@ -94,6 +73,8 @@ uint64_t *ds = (uint64_t *)disco_buf;
       uint8_t counter8 = 137;
 
       for( int Len = len >> 3; index < Len; index++) {
+        printf("rds1 = %#018" PRIx64 " %#018" PRIx64 " %#018" PRIx64 " %#018" PRIx64 "\n",
+          ds[0], ds[1], ds[2], ds[3] );
         ds[sindex] += rot(m64[index] + index + counter + 1, 23);
         counter += ~m64[index] + 1;
         if ( sindex == HSTATE64M ) {
@@ -102,6 +83,8 @@ uint64_t *ds = (uint64_t *)disco_buf;
           mix(2);
           sindex = -1;
         }
+        printf("rds2 = %#018" PRIx64 " %#018" PRIx64 " %#018" PRIx64 " %#018" PRIx64 "\n",
+          ds[0], ds[1], ds[2], ds[3] );
         sindex++;
       }
 
@@ -132,9 +115,9 @@ uint64_t *ds = (uint64_t *)disco_buf;
       const uint8_t *key8Arr = (uint8_t *)key;
       const uint64_t *key64Arr = (uint64_t *)key;
 
-      const uint8_t seedbuf[16] = {0};
-      const uint8_t *seed8Arr = (uint8_t *)seedbuf;
-      const uint64_t *seed64Arr = (uint64_t *)seedbuf;
+      uint8_t seedbuf[16] = {0};
+      uint8_t *seed8Arr = (uint8_t *)seedbuf;
+      uint64_t *seed64Arr = (uint64_t *)seedbuf;
       uint32_t *seed32Arr = (uint32_t *)seedbuf;
 
       // the cali number from the Matrix (1999)
@@ -151,6 +134,9 @@ uint64_t *ds = (uint64_t *)disco_buf;
       ds[2] = 0xaccadacca80081e5;
       ds[3] = 0xf00baaf00f00baaa;
 
+      printf("ds = %#018" PRIx64 " %#018" PRIx64 " %#018" PRIx64 " %#018" PRIx64 "\n",
+        ds[0], ds[1], ds[2], ds[3] );
+
       round( key64Arr, key8Arr, len );
       round( seed64Arr, seed8Arr, 16 );
       round( ds, ds8, STATE   );
@@ -160,10 +146,9 @@ uint64_t *ds = (uint64_t *)disco_buf;
         ds[0], ds[1], ds[2], ds[3] );
       **/
 
-      const uint8_t output[STATE] = {0};
+      uint8_t output[STATE] = {0};
       uint64_t *h = (uint64_t *)output;
 
-      //h[0] = ds[1];
       h[0] = ds[2];
       h[1] = ds[3];
 
