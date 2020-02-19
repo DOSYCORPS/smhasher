4312dee4aaa371a9c427a63b9c1999faf0c03e91
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-02-19 09:06:34 +0000

    "OpenMP test.
    Much slower. Very funny."

--- discohash.cpp
+++ discohash.cpp
@@ -10,8 +10,6 @@
 
 #define FORCE_INLINE  __forceinline
 
-// Other compilers
-
 #else // defined(_MSC_VER)
 
 #define FORCE_INLINE inline __attribute__((always_inline))
@@ -23,9 +21,10 @@ const int STATE64 = STATE >> 3;
 const int STATEM = STATE-1;
 const int HSTATE64M = (STATE64 >> 1)-1;
 const int STATE64M = STATE64-1;
+const uint64_t P = 0xFFFFFFFFFFFFFFFF - 58;
+const uint64_t Q = 13166748625691186689U;
+
 uint8_t disco_buf[STATE] = {0};
-uint64_t P = 0xFFFFFFFFFFFFFFFF - 58;
-uint64_t Q = 13166748625691186689U;
 uint8_t *ds8 = (uint8_t *)disco_buf;
 uint32_t *ds32 = (uint32_t *)disco_buf;
 uint64_t *ds = (uint64_t *)disco_buf;
@@ -41,30 +40,12 @@ uint64_t *ds = (uint64_t *)disco_buf;
       return v; 
     }
 
-    FORCE_INLINE uint64_t rot32( uint64_t v, int n) 
-    {
-      n = n & 31U;
-      if (n)
-          v = (v >> n) | (v << (64-n));
-      return v; 
-    }
-
     FORCE_INLINE uint8_t rot8( uint8_t v, int n) 
     {
       n = n & 7U;
       if (n)
           v = (v >> n) | (v << (8-n));
       return v; 
-    }
-
-    FORCE_INLINE void mixA()
-    {
-      int i = ds32[0] & 1;
-      int j = ds32[3] & 3;
-
-      ds[0] = rot(ds[0], ds[i]);
-      ds[i] *= (P % (ds32[j] + 1) + 1);
-      ds[1] += ds32[j];
     }
 
     FORCE_INLINE void mix(const int A)
@@ -88,12 +69,14 @@ uint64_t *ds = (uint64_t *)disco_buf;
 
     FORCE_INLINE void round( const uint64_t * m64, const uint8_t * m8, int len )
     {
-      int index = 0;
+      int index;
       int sindex = 0;
+      int Len = len >> 3;
       uint64_t counter = 0xfaccadaccad09997;
       uint8_t counter8 = 137;
 
-      for( int Len = len >> 3; index < Len; index++) {
+      #pragma omp parallel for
+      for( index = 0; index < Len; index++) {
         ds[sindex] += rot(m64[index] + index + counter + 1, 23);
         counter += ~m64[index] + 1;
         if ( sindex == HSTATE64M ) {
@@ -107,9 +90,11 @@ uint64_t *ds = (uint64_t *)disco_buf;
 
       mix(1);
 
-      index <<= 3;
+      Len = index << 3;
       sindex = index&(STATEM);
-      for( ; index < len; index++) {
+      
+      #pragma omp parallel for
+      for( index = Len; index < len; index++) {
         ds8[sindex] += rot8(m8[index] + index + counter8 + 1, 23);
         counter8 += ~m8[sindex] + 1;
         mix(index%STATE64M);
@@ -140,7 +125,6 @@ uint64_t *ds = (uint64_t *)disco_buf;
       // the cali number from the Matrix (1999)
       seed32Arr[0] = 0xc5550690;
       seed32Arr[0] -= seed;
-      // if seed mod doesn't work let's try reverse order of seed/key round calls
       seed32Arr[1] = 1 + seed;
       seed32Arr[2] = ~(1 - seed);
       seed32Arr[3] = (1+seed) * 0xf00dacca;
@@ -163,7 +147,6 @@ uint64_t *ds = (uint64_t *)disco_buf;
       const uint8_t output[STATE] = {0};
       uint64_t *h = (uint64_t *)output;
 
-      //h[0] = ds[1];
       h[0] = ds[2];
       h[1] = ds[3];
 
